7d42767be95af7b631f0edef10dce1197a0bda44
Witek Pokorski <witoldp@joulertx.dyndns.cern.ch>
2026-01-07 17:37:02 +0100

    fixing the position of the particle gun

--- G4MScInThinLayerTest/src/PrimaryGeneratorAction.cc
+++ G4MScInThinLayerTest/src/PrimaryGeneratorAction.cc
@@ -76,7 +76,8 @@ void PrimaryGeneratorAction::SetDefaultKinematic()
   fParticleGun->SetParticleDefinition(particle);
   fParticleGun->SetParticleMomentumDirection(G4ThreeVector(1.,0.,0.));
   fParticleGun->SetParticleEnergy(30.*MeV);
-  G4double x0 = -0.5*(fDetector->GetWorldSizeX());
+  G4double x0 =  -0.4999*(fDetector->GetWorldSizeX());
+  std::cout << "x0 " << x0 << std::endl;
   fParticleGun->SetParticlePosition(G4ThreeVector(x0, 0.0, 0.0));  
 }
 
